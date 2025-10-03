d882795f48a27307b0b1c4453e953945d838d871
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-03 20:31:58 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,14 @@ import { Server } from "socket.io";
 import express from "express";
 
 
-
 const app = express();
 
 const server = createServer(app);
+
+app.get('/' , (req,res)=> {
+    res.send ('<h1>Hello Rorld </>')
+})
+
+server.listen(4000, ()=>{
+    console.log('Server is running on http://localhost:4000');
+});
