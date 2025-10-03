67a1e459e57ab3f4dd2130ad3a0e3b1b5943a803
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-03 21:33:00 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import {createServer} from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import express from "express";
 
@@ -6,10 +6,40 @@ const app = express();
 
 const server = createServer(app);
 
-app.get('/' , (req,res)=> {
-    res.send ('<h1>Hello Rorld </>')
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+
+const ROOM = 'group';
+
+io.on("connection" , (socket)=>{
+    console.log("A user connected SuccessFully" ,socket.id )
+
+    socket.on("joinRoom" , async(userName)=>{
+         console.log(`${userName} this user joined the group ✅`);
+         await socket.join(ROOM)
+         socket.to(ROOM).emit("roomNotice" , userName)
+    })
+
+    socket.on("chatMessage" , (msg)=>{
+      socket.to(ROOM).emit("chatMessage" , msg)
+    })
+
+      socket.on('typing', (userName) => {
+        socket.to(ROOM).emit('typing', userName);
+    });
+    socket.on('stopTyping', (userName) => {
+        socket.to(ROOM).emit('stopTyping', userName);
+    });
 })
 
-server.listen(4600, ()=>{
-    console.log('Server is running on http://localhost:4600');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world </h1>");
+});
+
+server.listen(4600, () => {
+  console.log("server running at http://localhost:4600");
 });
