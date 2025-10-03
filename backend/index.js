a1997015c3f6665523ebf6a9a0eb3a5bd40eb2b6
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-03 20:41:47 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,6 @@ app.get('/' , (req,res)=> {
     res.send ('<h1>Hello Rorld </>')
 })
 
-server.listen(4000, ()=>{
-    console.log('Server is running on http://localhost:4000');
+server.listen(4600, ()=>{
+    console.log('Server is running on http://localhost:4600');
 });
